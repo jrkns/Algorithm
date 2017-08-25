04088b9b7e94b4ac9a3464b06a4621ca411c11c2
jrkns <jrkns1996@gmail.com>
2017-08-25 22:35:45 +0700

    DFS BFS Added

---
+++ graph/bfs.py
@@ -0,0 +1,20 @@
+def bfs(graph, start):
+    visited, queue = set(), [start]
+    while queue:
+        vertex = queue.pop(0)
+        if vertex not in visited:
+            visited.add(vertex)
+            queue.extend(graph[vertex] - visited)
+    return visited
+
+def main():
+    graph = {'A': set(['B', 'C']),
+         'B': set(['A', 'D', 'E']),
+         'C': set(['A', 'F']),
+         'D': set(['B']),
+         'E': set(['B', 'F']),
+         'F': set(['C', 'E'])}
+    print(bfs(graph,'A'))
+
+if __name__ == '__main__':
+    main()
---
+++ graph/dfs.py
@@ -0,0 +1,20 @@
+def dfs(graph, start):
+    visited, stack = set(), [start]
+    while stack:
+        vertex = stack.pop()
+        if vertex not in visited:
+            visited.add(vertex)
+            stack.extend(graph[vertex] - visited)
+    return visited
+
+def main():
+    graph = {'A': set(['B', 'C']),
+         'B': set(['A', 'D', 'E']),
+         'C': set(['A', 'F']),
+         'D': set(['B']),
+         'E': set(['B', 'F']),
+         'F': set(['C', 'E'])}
+    print(dfs(graph,'A'))
+
+if __name__ == '__main__':
+    main()
